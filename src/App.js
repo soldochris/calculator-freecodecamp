fbbf2b1c2da778aaa3f079fa4900919eb4a15e77
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-10-15 18:33:50 -0500

    logo image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import inspiredCodeLogo from "./images/logo-large.png";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='inspiredCode-logo-container'>
+        <img 
+          src={inspiredCodeLogo}
+          className='inspiredCode-logo'
+          alt='Inspired Code logo'
+        />
+      </div>
     </div>
   );
 }
