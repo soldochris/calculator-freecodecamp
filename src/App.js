7b796575330455ae7b6057cf569ade2e9f6ab607
Christian Soldevilla <soldevilla.christian@gmail.com>
2023-10-15 21:22:08 -0500

    Button component and basic buttons layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Button from './components/Button';
 import inspiredCodeLogo from "./images/logo-large.png";
 
 function App() {
@@ -10,6 +11,35 @@ function App() {
           className='inspiredCode-logo'
           alt='Inspired Code logo'
         />
+      </div>
+      <div className='calculator-container'>
+        <div className='row'>
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>+</Button>
+        </div>
+        <div className='row'>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>-</Button>
+        </div>
+        <div className='row'>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>*</Button>
+        </div>
+        <div className='row'>
+          <Button>=</Button>
+          <Button>0</Button>
+          <Button>.</Button>
+          <Button>/</Button>
+        </div>
+        <div className='row'>
+
+        </div>
       </div>
     </div>
   );
